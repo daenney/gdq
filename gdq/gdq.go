15a55a29ee87cdebe24bc6fdf6b72a8e855758a4
Daniele Sluijters <daenney@users.noreply.github.com>
2020-12-05 18:11:21 +0100

    Introduce a proper schedule type
    A schedule contains the list of events but now also two maps that map
    runners and host to their respective events. This is going to be used to
    be able to quickly filter a schedule.
    
    * NewSchedule can be initialised as empty, or with a series of events
    * load can be used to update a schedule with new events. If you call load
      multiple times with the same series of events you're going to end up
      with duplicates
    * For(Host|Runner|Title) return a new schedule only containing events
      matching the host, runner or title

--- gdq/gdq.go
+++ gdq/gdq.go
@@ -4,10 +4,129 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/anaskhan96/soup"
 )
+
+// Schedule represents the events occurring at a GDQ
+type Schedule struct {
+	Events   []*Event
+	byRunner map[string][]*Event
+	byHost   map[string][]*Event
+	sync.RWMutex
+}
+
+// NewSchedule returns an empty Schedule
+func NewSchedule(events []*Event) *Schedule {
+	if events != nil && len(events) > 0 {
+		s := &Schedule{
+			Events:   make([]*Event, 0, len(events)),
+			byRunner: map[string][]*Event{},
+			byHost:   map[string][]*Event{},
+		}
+		s.load(events)
+		return s
+	}
+
+	return &Schedule{
+		Events:   []*Event{},
+		byRunner: map[string][]*Event{},
+		byHost:   map[string][]*Event{},
+	}
+}
+
+// load a series of events in the Schedule
+//
+// Call this method when wanting to add events to a schedule to ensure that
+// the byRunner and byHost maps get updated. This permits the filter functions
+// like ForHost and ForRunner to work
+func (s *Schedule) load(events []*Event) {
+	s.Lock()
+	defer s.Unlock()
+	for _, event := range events {
+		s.Events = append(s.Events, event)
+		for _, runner := range event.Runners {
+			rev, ok := s.byRunner[runner]
+			if ok {
+				s.byRunner[runner] = append(rev, event)
+			} else {
+				s.byRunner[runner] = []*Event{event}
+			}
+		}
+		for _, host := range event.Hosts {
+			hev, ok := s.byHost[host]
+			if ok {
+				s.byHost[host] = append(hev, event)
+			} else {
+				s.byHost[host] = []*Event{event}
+			}
+		}
+	}
+}
+
+// ForRunner returns a new schedule with events only matching this runner
+//
+// The runner's name is matched using a string submatch. This means that if you
+// call somtething like schedule.ForRunner("b") you can get a schedule with events
+// for multiple runners.
+//
+// The match is case insensitive.
+func (s *Schedule) ForRunner(name string) *Schedule {
+	s.RLock()
+	defer s.RUnlock()
+
+	ns := NewSchedule(nil)
+	for r := range s.byRunner {
+		if strings.Contains(strings.ToLower(r), strings.ToLower(name)) {
+			ns.load(s.byRunner[r])
+		}
+	}
+	return ns
+}
+
+// ForHost returns a new schedule with events only matching this host
+//
+// The host's name is matched using a string submatch. This means that if you
+// call somtething like schedule.ForHust("b") you can get a schedule with events
+// for multiple hosts.
+//
+// The match is case insensitive.
+func (s *Schedule) ForHost(name string) *Schedule {
+	s.RLock()
+	defer s.RUnlock()
+
+	ns := NewSchedule(nil)
+	for h := range s.byHost {
+		if strings.Contains(strings.ToLower(h), strings.ToLower(name)) {
+			ns.load(s.byHost[h])
+		}
+	}
+	return ns
+}
+
+// ForTitle returns a new schedule with events only matching this event title
+//
+// The title is matched using a string submatch. This means that if you call
+// somtething like schedule.ForTitle("b") you can get a schedule with multiple
+// events.
+//
+// The match is case insensitive.
+func (s *Schedule) ForTitle(title string) *Schedule {
+	s.RLock()
+	defer s.RUnlock()
+
+	evs := []*Event{}
+	for _, e := range s.Events {
+		if strings.Contains(strings.ToLower(e.Title), strings.ToLower(title)) {
+			evs = append(evs, e)
+		}
+	}
+
+	ns := NewSchedule(evs)
+	return ns
+}
 
 // Event represents a single event at a GDQ
 type Event struct {
@@ -21,8 +140,8 @@ type Event struct {
 	Platform string
 }
 
-// GetSchedule returns a list of GDQ events for a GDQ edition
-func GetSchedule(id Edition, client *http.Client) ([]*Event, error) {
+// GetSchedule returns the Schedule for a GDQ edition
+func GetSchedule(id Edition, client *http.Client) (*Schedule, error) {
 	if client == nil {
 		client = newHTTPClient()
 	}
@@ -51,7 +170,9 @@ func GetSchedule(id Edition, client *http.Client) ([]*Event, error) {
 		events = append(events, event)
 	}
 
-	return events, nil
+	schedule := NewSchedule(events)
+
+	return schedule, nil
 }
 
 func createEvent(rows ...soup.Root) (*Event, error) {
--- main.go
+++ main.go
@@ -16,16 +16,21 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
-	fmt.Fprintln(w, "Start Time\tTitle\tEstimate\tRunners\tHosts")
-	for _, event := range schedule {
-		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n",
-			event.Start.Local().Format(time.Stamp),
-			event.Title,
-			event.Estimate,
-			strings.Join(event.Runners, ", "),
-			strings.Join(event.Hosts, ", "),
-		)
+
+	if schedule != nil && len(schedule.Events) > 0 {
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
+		fmt.Fprintln(w, "Start Time\tTitle\tEstimate\tRunners\tHosts")
+		schedule.RLock()
+		defer schedule.RUnlock()
+		for _, event := range schedule.Events {
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\n",
+				event.Start.Local().Format(time.Stamp),
+				event.Title,
+				event.Estimate,
+				strings.Join(event.Runners, ", "),
+				strings.Join(event.Hosts, ", "),
+			)
+		}
+		w.Flush()
 	}
-	w.Flush()
 }
