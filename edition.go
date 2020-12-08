44a084e7b2eb2e1c37a1cfa3be0c52b8dbcfdf0a
Daniele Sluijters <daenney@users.noreply.github.com>
2020-12-09 00:02:30 +0100

    edition: Mark FleetFatales2020 as an Edition

--- edition.go
+++ edition.go
@@ -5,7 +5,7 @@ type Edition uint
 
 const (
 	// FleetFatales2020 is the Fleet Fatales edition in November 2020
-	FleetFatales2020 = 33
+	FleetFatales2020 Edition = 33
 	// AGDQ2021 is the GDQ in January 2021
 	AGDQ2021 Edition = 34
 )
