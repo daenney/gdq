82fac1f82a14947145a638d945affeec8bd35d29
Daniele Sluijters <daenney@users.noreply.github.com>
2020-12-11 19:34:04 +0100

    edition: Add additional test for schedule ID

--- edition_test.go
+++ edition_test.go
@@ -15,28 +15,30 @@ func TestEdition(t *testing.T) {
 		var tests = []struct {
 			edition Edition
 			name    string
+			id      uint
 		}{
-			{edition: AGDQ2016, name: "AGDQ2016"},
-			{edition: SGDQ2016, name: "SGDQ2016"},
-			{edition: AGDQ2017, name: "AGDQ2017"},
-			{edition: SGDQ2017, name: "SGDQ2017"},
-			{edition: HRDQ2017, name: "HRDQ2017"},
-			{edition: AGDQ2018, name: "AGDQ2018"},
-			{edition: SGDQ2018, name: "SGDQ2018"},
-			{edition: GDQX2018, name: "GDQX2018"},
-			{edition: AGDQ2019, name: "AGDQ2019"},
-			{edition: SGDQ2019, name: "SGDQ2019"},
-			{edition: GDQX2019, name: "GDQX2019"},
-			{edition: AGDQ2020, name: "AGDQ2020"},
-			{edition: FrostFatales2020, name: "FrostFatales2020"},
-			{edition: SGDQ2020, name: "SGDQ2020"},
-			{edition: CRDQ2020, name: "CRDQ2020"},
-			{edition: FleetFatales2020, name: "FleetFatales2020"},
-			{edition: AGDQ2021, name: "AGDQ2021"},
+			{edition: AGDQ2016, name: "AGDQ2016", id: 17},
+			{edition: SGDQ2016, name: "SGDQ2016", id: 18},
+			{edition: AGDQ2017, name: "AGDQ2017", id: 19},
+			{edition: SGDQ2017, name: "SGDQ2017", id: 20},
+			{edition: HRDQ2017, name: "HRDQ2017", id: 21},
+			{edition: AGDQ2018, name: "AGDQ2018", id: 22},
+			{edition: SGDQ2018, name: "SGDQ2018", id: 23},
+			{edition: GDQX2018, name: "GDQX2018", id: 24},
+			{edition: AGDQ2019, name: "AGDQ2019", id: 25},
+			{edition: SGDQ2019, name: "SGDQ2019", id: 26},
+			{edition: GDQX2019, name: "GDQX2019", id: 27},
+			{edition: AGDQ2020, name: "AGDQ2020", id: 28},
+			{edition: FrostFatales2020, name: "FrostFatales2020", id: 29},
+			{edition: SGDQ2020, name: "SGDQ2020", id: 30},
+			{edition: CRDQ2020, name: "CRDQ2020", id: 31},
+			{edition: FleetFatales2020, name: "FleetFatales2020", id: 33},
+			{edition: AGDQ2021, name: "AGDQ2021", id: 34},
 		}
 		for _, tt := range tests {
 			t.Run(tt.name, func(t *testing.T) {
 				assertEqual(t, tt.edition.String(), tt.name)
+				assertEqual(t, uint(tt.edition), tt.id)
 			})
 		}
 	})
