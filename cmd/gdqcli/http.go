061ac4b8e13cd1ede00455ef2f3a2dcfd7024482
Daniele Sluijters <daenney@users.noreply.github.com>
2020-12-16 16:00:06 +0100

    http: http.DefaultTransport is fine
    My brain confused some issues with http.DefaultServer vs
    http.DefaultTransport. DefaultTransport is fine, so use that instead of
    defining our own with the same settings.

--- cmd/gdqcli/http.go
+++ cmd/gdqcli/http.go
@@ -1,32 +1,16 @@
 package main
 
 import (
-	"net"
 	"net/http"
-	"time"
 )
 
 const userAgent = "gdqcli (+https://github.com/daenney/gdq)"
-
-var defaultTrasnport = &http.Transport{
-	Proxy: http.ProxyFromEnvironment,
-	DialContext: (&net.Dialer{
-		Timeout:   30 * time.Second,
-		KeepAlive: 30 * time.Second,
-		DualStack: true,
-	}).DialContext,
-	MaxIdleConns:          5,
-	IdleConnTimeout:       90 * time.Second,
-	TLSHandshakeTimeout:   10 * time.Second,
-	ExpectContinueTimeout: 1 * time.Second,
-	MaxIdleConnsPerHost:   2,
-}
 
 type transport struct{}
 
 func (*transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Set("User-Agent", userAgent)
-	return defaultTrasnport.RoundTrip(req)
+	return http.DefaultTransport.RoundTrip(req)
 }
 
 func newHTTPClient() *http.Client {
